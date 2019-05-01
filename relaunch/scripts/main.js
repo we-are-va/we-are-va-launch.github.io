38d2911d039acb5ad463fc17a5c79890c5764349
Skye Johnson <skye@thevariousartists.com.au>
2019-05-02 09:33:03 +1000

    Relaunch updates

--- relaunch/scripts/main.js
+++ relaunch/scripts/main.js
@@ -1,95 +1,74 @@
-console.log("Hello!");
+console.log("Hello, welcome to VA Relaunch!");
 
 
-
-// Test artwork5
-// const myVivus1 = new Vivus('artwork5', { 
-//   duration: 500, 
-//   start: 'autostart',
-//   pathTimingFunction: Vivus.LINEAR,
-//   type: 'sync',
-//   file: '/images/artwork1.svg'
-//   },
-//   //next
-// );
-// const myVivus2 = new Vivus('artwork5', { 
-//   duration: 500, 
-//   start: 'autostart',
-//   pathTimingFunction: Vivus.LINEAR,
-//   type: 'sync',
-//   file: '/images/artwork2.svg'
-//   }//,
-//   //next
-// );
-// const myVivus3 =  new Vivus('artwork5', { 
-//   duration: 500, 
-//   start: 'autostart',
-//   pathTimingFunction: Vivus.LINEAR,
-//   type: 'sync',
-//   file: '/images/artwork1.svg'
-//   }//,
-//   //next
-// );
-
-// myVivus1.play(function() {
-//   myVivus2
-// });
-
-
-// Looping code
-var index = 1;
-var max = 6;
-function run(){
-  dovivus();
+var svg_el = document.getElementsByClassName("artwork");
+var current_svg  = 0; // Current SVG in rotation
+var max_svgs     = svg_el.length; // Amount of SVGS
+var i = 0, svgs = [];
+var pathType = "sync";
+var duration = 2000;
+var background = "";
+for (i = 0; i < svg_el.length; i++) {
+  //console.log(svg_el[i].getAttribute("id"));
+  svgs[i] = svg_el[i];
 }
-function next( ){
-  document.getElementById('artwork' + index).style.opacity = 0;
-  index++;
-  if(index == max) {
-    index = 1;
-    return;  
-  } else{
-    dovivus();
-  }
-} 
 
-function dovivus(){
-  console.log("Do vivus! --> " + index);
-  new Vivus('artwork' + index, { 
-    duration: 700, 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+// Go to next SVG
+function next( ){
+
+  svg_el[current_svg].style.opacity = 0;
+
+  // stop for sometime if needed
+  setTimeout(function() {
+   // svg_el[current_svg].style.opacity = 0;
+    current_svg++;
+    console.log(current_svg);
+    if(current_svg == max_svgs) {
+      current_svg = 0;
+    } 
+    animate_svg();
+  }, 5000);
+} 
+  
+function animate_svg(){
+  console.log("Do vivus! --> Artwork #" + current_svg);
+  svg_el[current_svg].style.opacity = 1;
+  
+  pathType = svg_el[current_svg].getAttribute("data-path-type");
+  if(!pathType) { pathType = "sync"; }
+
+  duration = svg_el[current_svg].getAttribute("data-duration");
+  if(!duration) { duration = 2000; } 
+
+  background = svg_el[current_svg].getAttribute("data-background");
+  if(background) { document.getElementsByTagName("BODY")[0].style.backgroundColor = "#" + background; }
+  else { document.getElementsByTagName("BODY")[0].style.backgroundColor = "#fff"; }
+
+
+  new Vivus(svg_el[current_svg].getAttribute("id"), { 
+    duration: duration,
     start: 'autostart',
-    pathTimingFunction: Vivus.LINEAR,
-    type: 'sync',
-    file: 'images/artwork' + index + '.svg',
+    pathTimingFunction: Vivus.EASE,
+    type: pathType, //'sync', 'oneByOne',
+    file: '/images/artwork' + current_svg + '.svg',
     // onReady: function (myVivus) {
     //     // `el` property is the SVG element
-    //     document.getElementById('artwork' + index).style.opacity = 1;//('opacity', '1');
+    //     document.getElementById('artwork' + current_svg).style.opacity = 1;//('opacity', '1');
     //   }
     },
     next
   );
 } 
 
-run();  
-
-// const myVivus = new Vivus('artwork');
-
-// function callback () { console.log('all done'); }
-
-// var i;
-// for (i = 1; i < 7; i++) {
-//   console.log("i is " + i);
-//   new Vivus('artwork', { 
-//     duration: 700, 
-//     start: 'autostart',
-//     pathTimingFunction: Vivus.LINEAR,
-//     type: 'oneByOne',
-//     file: '/images/artwork' + i + '.svg' 
-//   });
-// }
-
-
-// const myVivus = new Vivus('my-svg-id');
-// myVivus.play(1, function() {
-//   // called after the animation completes
-// });
+// **************** 
+// Run the Vivus SVG code
+animate_svg(); 
